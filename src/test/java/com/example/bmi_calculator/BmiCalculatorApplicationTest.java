8a0f8b18f75aed4b398504743c11c6fb3cc0cf28
karMANdOwn <karmandown@gmail.com>
2025-01-09 17:12:07 +0100

    javitások

--- src/test/java/com/example/bmi_calculator/BmiCalculatorApplicationTest.java
+++ src/test/java/com/example/bmi_calculator/BmiCalculatorApplicationTest.java
@@ -1,12 +1,24 @@
 package com.example.bmi_calculator;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
+@TestPropertySource(properties = {
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1"
+})
 class BmiCalculatorApplicationTest {
 
     @Test
+    void contextLoads() {
+
+    }
+
+    @Test
     void main() {
+
+        BmiCalculatorApplication.main(new String[]{});
     }
 }
